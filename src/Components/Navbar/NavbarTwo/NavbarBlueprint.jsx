9490ec48fedaa159e199e52d2440d9fda4c34ab1
johanMap2u <johan@map2u.com.my>
2023-12-19 23:53:27 +0800

    Update Navbar

--- src/Components/Navbar/NavbarTwo/NavbarBlueprint.jsx
+++ src/Components/Navbar/NavbarTwo/NavbarBlueprint.jsx
@@ -1,100 +1,144 @@
-import {
-  Button,
-  Navbar,
-  NavbarGroup,
-  Alignment,
-  Classes,
-} from "@blueprintjs/core";
-import React from "react";
+import { Button, Classes, Position, Drawer, H1 } from "@blueprintjs/core";
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import "./NavbarBlueprint.css";
-import beer from "../../assets/beer.png";
-const navbarStyle = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  padding: "10px",
-  boxShadow: "0 1px 5px rgba(0, 0, 0, 0.1)",
-  backgroundColor: "#f5f5f5",
+
+export const NavbarBlueprint = ({
+	DarkMode,
+	MenuPosition,
+	label,
+	ImageLogo,
+	ImgStatus,
+	ListNav = [],
+}) => {
+	const Dark = DarkMode ? "bp5-dark" : "bp5-light";
+	const Menupost = MenuPosition ? "start" : "end";
+	const [isOpen, setIsOpen] = useState(false);
+	const toggleDrawer = () => {
+		setIsOpen((prevState) => !prevState);
+	};
+	useEffect(() => {
+		console.log(isOpen);
+	}, [isOpen]);
+	return (
+		<nav
+			className={["bp5-navbar", Dark].join(" ")}
+			style={{
+				borderRadius: "5px",
+			}}
+		>
+			<div
+				style={{
+					display: "flex",
+					width: "100%",
+					alignItems: "center",
+					height: "100%",
+				}}
+			>
+				{ImgStatus && (
+					<img
+						src={ImageLogo}
+						alt="logo"
+						style={{
+							height: "30px",
+							marginRight: "5px",
+						}}
+					/>
+				)}
+				<div class="bp5-navbar-group bp5-align-left">
+					<div class="bp5-navbar-heading">{label}</div>
+					{/* <input  class="bp5-input" placeholder="Search files..." type="text" /> */}
+				</div>
+				<div //desktop id
+					className="hide-on-mobile"
+					style={{
+						width: "100%",
+					}}
+				>
+					<div
+						style={{
+							width: "100%",
+							display: "flex",
+							// justifyContent: Menupost,
+							alignItems: "center",
+							// backgroundColor: "white",
+						}}
+					>
+						<div
+							style={{
+								display: "flex",
+								width: "100%",
+								justifyContent: Menupost,
+							}}
+						>
+							{/* <Button style={{ marginRight: "2px" }} icon="home" text="Home" />
+							<Button style={{ marginRight: "2px" }} icon="home" text="Home" /> */}
+							{ListNav.map((item, index) => (
+								<a
+									key={index}
+									href={item.link}
+									className={[
+										"bp5-button bp5-minimal",
+										`bp5-icon-${item.icon}`,
+									].join(" ")}
+								>
+									{item.title}
+								</a>
+							))}
+						</div>
+
+						<div style={{ width: "30%" }}>
+							<span class="bp5-navbar-divider"></span>
+							<Button
+								style={{ marginRight: "2px" }}
+								className={"bp5-button bp5-minimal bp5-icon-user"}
+							/>
+							<Button
+								style={{ marginRight: "2px" }}
+								className={"bp5-button bp5-minimal bp5-icon-cog"}
+							/>
+							<Button
+								style={{ marginLeft: "5px" }}
+								className={Classes.MINIMAL}
+								icon="document"
+								text="Logout"
+								onClick={()=>""}
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="bp5-navbar-group bp5-align-right burger-icon">
+					<Button
+						className={"bp5-button bp5-minimal bp5-icon-menu"}
+						onClick={() => setIsOpen((prevState) => !prevState)}
+					/>
+				</div>
+			</div>
+			{isOpen && (
+				<Drawer
+					isOpen={isOpen}
+					onClose={toggleDrawer}
+					title="Palantir Foundry"
+					position={Position.TOP}
+				>
+					{/* Content inside the drawer */}
+					<div style={{ padding: "20px" }}>
+						<h3>Drawer Content</h3>
+						<p>Any content you want to show in the drawer.</p>
+					</div>
+				</Drawer>
+			)}
+		</nav>
+	);
 };
 
-export const NavbarBlueprint = ({ Dark = true }) => {
-  const DarkMode = Dark ? "bp5-dark" : "bp5-light";
-  return (
-    <nav
-      className={["bp5-navbar", DarkMode].join(" ")}
-      //   className={["bp5-navbar","bp5-dark"].join(" ")}
-      style={{
-        // display: "block",
-        borderRadius: "5px",
-        // alignSelf:'center'
-        // margin: "0 auto"
-        // alignItems: "right",
-        // backgroundColor: "red",
-        // padding: "5px",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-          alignItems: "center",
-          height: "100%",
-        }}
-      >
-        <img
-          src={beer}
-          alt="logo"
-          style={{
-            height: "30px",
-            marginRight: "5px",
-          }}
-        />
-        <div class="bp5-navbar-group bp5-align-left">
-          <div class="bp5-navbar-heading">{"TitleNavbar"}</div>
-          {/* <input  class="bp5-input" placeholder="Search files..." type="text" /> */}
-        </div>
-        <div //desktop id
-          className="hide-on-mobile"
-          style={{
-            width: "100%",
-          }}
-        >
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              alignContent: "center",
-              justifyContent: "end", //change position
-              alignItems: "center",
-              marginLeft: "5px",
-              //   backgroundColor:'red'
-            }}
-            
-          >
-            <Button style={{ marginRight: "2px" }} icon="home" text="Home" />
-            <Button style={{ marginRight: "2px" }} icon="home" text="Home" />
+NavbarBlueprint.defaultProps = {
+	DarkMode: true,
+	MenuPosition: false,
+};
 
-            <span class="bp5-navbar-divider"></span>
-            <Button
-              style={{ marginRight: "2px" }}
-              className={"bp5-button bp5-minimal bp5-icon-user"}
-            />
-            <Button
-              style={{ marginRight: "2px" }}
-              className={"bp5-button bp5-minimal bp5-icon-cog"}
-            />
-            <Button
-              style={{ marginLeft: "10px" }}
-              className={Classes.MINIMAL}
-              icon="document"
-              text="Logout"
-            />
-          </div>
-        </div>
-        <div className="bp5-navbar-group bp5-align-right burger-icon">
-          <Button className={"bp5-button bp5-minimal bp5-icon-menu"} />
-        </div>
-      </div>
-    </nav>
-  );
+NavbarBlueprint.propTypes = {
+	DarkMode: PropTypes.bool,
+	MenuPosition: PropTypes.bool,
+	ImgStatus: PropTypes.bool,
 };
--- src/Components/Navbar/NavbarTwo/NavbarBlueprint.stories.jsx
+++ src/Components/Navbar/NavbarTwo/NavbarBlueprint.stories.jsx
@@ -1,11 +1,41 @@
 import { NavbarBlueprint } from "./NavbarBlueprint";
+import beer from "../../assets/beer.png";
+export default {
+	title: "Component/Navbars/NavbarTwo",
+	component: NavbarBlueprint,
+	tags: ["autodocs"],
+	parameters: {
+		docs: {
+			description: {
+				component: "Navbar description.",
+			},
+		},
+	},
+	argTypes: {
+		DarkMode: {
+			control: { type: "boolean", description: "Enable or disable dark mode." },
+		},
+		MenuPosition: {
+			control: { type: "boolean", description: "Enable or disable dark mode." },
+		},
+		ImgStatus: {
+			control: { type: "boolean" },
+		},
+		ListNav: { control: "array" },
+	},
+};
 
+const Template = (args) => <NavbarBlueprint {...args} />;
 
-export default{
-    title:"Component/Navbars/NavbarTwo",
-    component:NavbarBlueprint
-}
-
-const Template = (args)=> <NavbarBlueprint/>
-
-export const Navbar =Template.bind({})
+export const Navbar = Template.bind({});
+// https://en.wikipedia.org/static/images/icons/wikipedia.png
+Navbar.args = {
+	label: "NavBar",
+	ImageLogo: beer,
+	DarkMode: true,
+    ListNav: [
+        { title: "Home", link: "#", icon: "home" },
+        { title: "File", link: "#", icon: "document" },
+        { title: "Dashboard", link: "#", icon: "chart" },
+      ],
+};
